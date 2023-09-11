da8fc3ca6fc7634502b319aed9cdc55c72f82089
Anderson <andyguaca@gmail.com>
2023-09-11 10:45:30 -0500

    login page and register with angular material

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MaterialModule } from '../material/material.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
@@ -15,7 +15,8 @@ import { RegisterPagesComponent } from './pages/register-pages/register-pages.co
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
