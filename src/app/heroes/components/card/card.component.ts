e8d2471e5bb16f83821b96a1cc80792183baf814
Anderson <andyguaca@gmail.com>
2023-11-14 12:17:14 -0500

    construction of the heroes card list

---
+++ src/app/heroes/components/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interce';
+
+@Component({
+  selector: 'heroes-hero-card',
+  templateUrl: './card.component.html',
+  styles: [
+  ]
+})
+export class CardComponent implements OnInit{
+    @Input()
+    public hero!:Hero;
+    
+    ngOnInit(): void {
+        if (!this.hero) throw Error('Hero property is required.');
+    }
+}
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -8,6 +8,7 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { AddNewHeroPageComponent } from './pages/add-new-hero-page/add-new-hero-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { HeroPageComponent } from './pages/hero-page/hero-page.component';
     ListPageComponent,
     AddNewHeroPageComponent,
     SearchPageComponent,
-    HeroPageComponent
+    HeroPageComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
