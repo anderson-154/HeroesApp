2c2dea7d43af7afe45c2c285de8c1c7f11043983
Anderson <andyguaca@gmail.com>
2023-11-14 10:46:46 -0500

    implent service and connect with backend to show heroes in list page

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interce';
+import { Heroservice } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
@@ -8,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPageComponent implements OnInit {
 
-  constructor() { }
+  public heroes:Hero[] = [];
+
+  constructor(private heroservice:Heroservice) { }
 
   ngOnInit(): void {
+    this.heroservice.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
