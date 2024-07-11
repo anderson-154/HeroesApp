86817280afe9654aa26867fbec4fef8f1c8c5f4a
Anderson <andyguaca@gmail.com>
2024-07-11 12:36:29 -0500

    add CRUD

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable, catchError, of } from "rxjs";
+import { Observable, catchError, map, of } from "rxjs";
 import { environments } from "environments/environments";
-import { Hero } from "../interfaces/hero.interce";
+import { Hero } from '../interfaces/hero.interce';
 
 @Injectable({providedIn:'root'})
 export class Heroservice {
@@ -22,5 +22,21 @@ export class Heroservice {
 
     getSuggestions(query:string):Observable<Hero[]>{
       return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+    }
+
+    addHero(hero:Hero): Observable<Hero>{
+      return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+    }
+
+    updateHero(hero:Hero): Observable<Hero>{
+      if(!hero.id) throw Error("El heroe no existe");
+      return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+    }
+
+    deleteHeroById(id:string): Observable<boolean>{
+      return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError(err=> of(false)),
+        map( resp=>true)
+      );
     }
 }
